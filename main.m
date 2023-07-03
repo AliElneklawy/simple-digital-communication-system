04cc43842dafed7a018e3f2c83f2dc0c42b21537
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2023-07-03 15:27:52 +0300

    Update main.m

--- main.m
+++ main.m
@@ -5,3 +5,8 @@ expected_seq = hard_decision_decoding(noisy_seq);
 fprintf('The transmitted sequence is: %s\n', seq)
 fprintf('The noisy sequence is: %s\n', noisy_seq)
 fprintf('The expected sequence is: %s\n', expected_seq{1})
+
+%TODO
+%  use the generator matrix at the transmitter to generate a codeword
+%  use the parity check matrix at the receiver to decode the received sequences
+%  use syndrome vector for error detection and correction
